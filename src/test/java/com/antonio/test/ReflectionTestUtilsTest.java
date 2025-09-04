2e609af3bde79f668b9d6943296cfb036691b791
Antonio Espinoza <aespinozareyes9@gmail.com>
2025-09-04 17:02:57 -0400

    add id field and reflection tests for CollegeStudent class

---
+++ src/test/java/com/antonio/test/ReflectionTestUtilsTest.java
@@ -0,0 +1,54 @@
+package com.antonio.test;
+
+import com.antonio.component.MvcTestingExampleApplication;
+import com.antonio.component.models.CollegeStudent;
+import com.antonio.component.models.StudentGrades;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class ReflectionTestUtilsTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    CollegeStudent collegeStudent;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+    @BeforeEach
+    public void studentBeforeEach() {
+        collegeStudent.setFirstname("Antonio");
+        collegeStudent.setLastname("Espinoza");
+        collegeStudent.setEmailAddress("antoni@gmail.com");
+        collegeStudent.setStudentGrades(studentGrades);
+
+        ReflectionTestUtils.setField(collegeStudent, "id", 1);
+        ReflectionTestUtils.setField(collegeStudent, "studentGrades",
+                new StudentGrades(new ArrayList<>(Arrays.asList(
+                        100.0, 85.0, 76.50, 91.75
+                ))));
+    }
+
+    @Test
+    public void getPrivateField(){
+        assertEquals(1, ReflectionTestUtils.getField(collegeStudent, "id"));
+    }
+
+    @Test
+    public void invokePrivateMethod(){
+        assertEquals("Antonio 1", ReflectionTestUtils.invokeMethod(collegeStudent, "getFirstNameAndId"),
+                "Fail private method not call");
+    }
+}
