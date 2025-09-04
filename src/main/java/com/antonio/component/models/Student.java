66885e35f133df955a2494b1acc57f2a0069a9cd
Antonio Espinoza <aespinozareyes9@gmail.com>
2025-09-04 16:17:38 -0400

    first commit

---
+++ src/main/java/com/antonio/component/models/Student.java
@@ -0,0 +1,8 @@
+package com.antonio.component.models;
+
+public interface Student {
+
+    String studentInformation();
+
+    String getFullName();
+}
---
+++ src/main/java/com/antonio/component/service/ApplicationService.java
@@ -0,0 +1,26 @@
+package com.antonio.component.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.antonio.component.dao.ApplicationDao;
+
+import java.util.List;
+
+public class ApplicationService {
+
+    @Autowired
+    private ApplicationDao applicationDao;
+
+    public double addGradeResultsForSingleClass(List<Double> numbers) {
+        return applicationDao.addGradeResultsForSingleClass(numbers);
+    }
+
+    public double findGradePointAverage (List<Double> grades ) {
+        return applicationDao.findGradePointAverage(grades);
+    }
+
+    public Object checkNull(Object obj) {
+        return applicationDao.checkNull(obj);
+    }
+
+}
